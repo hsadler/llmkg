bd5b4c6d9dc9c7892b6c4583abcb37d70df68f50
hsadler <harrysadlermusic@gmail.com>
2024-05-18 19:20:38 +0200

    refactor api for subject to reduce to only 2 endpoints

--- backend/app/routers/subjects.py
+++ backend/app/routers/subjects.py
@@ -14,8 +14,15 @@ logger = logging.getLogger(__name__)
 router: APIRouter = APIRouter(prefix="/api/subjects", tags=["subjects"])
 
 
+class Subject(BaseModel):
+    name: str = Field(description="Subject name.", example="cooking")
+    related_subjects: list[str] = Field(
+        description="Related subject names.", example=["baking", "sushi"]
+    )
+
+
 class APISubjectOutput(BaseModel):
-    data: subjects_repo.SubjectRecord = Field(description="Subject found or created.")
+    data: Subject = Field(description="Subject found or created.")
     meta: dict[str, Any] = Field(description="Metadata about the subject.")
 
 
@@ -31,14 +38,24 @@ async def find_or_create_subject_by_name(
 ) -> APISubjectOutput:
     logger.info("Finding or creating new subject", extra={"subject_name": subject_name})
     try:
-        subject = await subjects_repo.fetch_subject_by_name(db, subject_name=subject_name)
-        if subject is not None:
+        subject_record = await subjects_repo.fetch_subject_by_name(db, subject_name=subject_name)
+        if subject_record is not None:
             # Existing subject found
-            logger.info("Subject found", extra={"subject": dict(subject)})
+            logger.info("Subject found", extra={"subject_record": dict(subject_record)})
         else:
             # Create new subject
-            subject = await subjects_repo.create_subject(db, subject_name=subject_name)
-            logger.info("Subject created", extra={"subject": dict(subject)})
+            subject_record = await subjects_repo.create_subject(db, subject_name=subject_name)
+            logger.info("Subject created", extra={"subject_record": dict(subject_record)})
+        logger.info(
+            "Fetching related subjects by subject name", extra={"subject_name": subject_name}
+        )
+        subject_relation_records = await subjects_repo.fetch_subject_relations_by_subject_name(
+            db, subject_name
+        )
+        subject = Subject(
+            name=subject_record.name,
+            related_subjects=[r.related_subject_name for r in subject_relation_records],
+        )
         return APISubjectOutput(data=subject, meta={})
     except Exception as e:
         logger.exception(
@@ -93,35 +110,3 @@ async def create_related_subjects(
         )
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
     return Response(status_code=status.HTTP_201_CREATED)
-
-
-class APIRelatedSubjectsOutput(BaseModel):
-    data: list[str] = Field(description="Related Subjects found.")
-    meta: dict[str, Any] = Field(description="Metadata about the related subjects.")
-
-
-@router.get(
-    "/related-subjects/{subject_name}",
-    description="Get related subjects by subject name.",
-    responses={
-        status.HTTP_404_NOT_FOUND: {"description": "Resource not found"},
-    },
-    status_code=status.HTTP_200_OK,
-)
-async def get_related_subjects_by_subject_name(
-    subject_name: str, db: Database = Depends(get_database)
-) -> APIRelatedSubjectsOutput:
-    logger.info("Fetching related subjects by subject name", extra={"subject_name": subject_name})
-    try:
-        subject_relation_records = await subjects_repo.fetch_subject_relations_by_subject_name(
-            db, subject_name
-        )
-    except Exception as e:
-        logger.exception(
-            "Error fetching related subjects by subject name",
-            extra={"subject_name": subject_name, "error": e},
-        )
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    return APIRelatedSubjectsOutput(
-        data=[r.related_subject_name for r in subject_relation_records], meta={}
-    )
